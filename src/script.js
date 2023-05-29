301f6bdcaee10023d0b3524b5b46899cc9b2e4e0
FiveXC <120128140+FiveXC@users.noreply.github.com>
2023-05-29 10:08:39 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,64 +1,55 @@
 import './styles.css'
-import  TextoBtn from './componentes/scriptReact'
 import React, {useState,useEffect} from 'react'
-
-async function baralhoDeCarta(){
-  try{
-  
-  let baralho = await new Promise(async function (resolve, reject) {
-     
-      let itemDaApi = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-      
-      if(itemDaApi.ok) {
-          let apiFormatada = await itemDaApi.json();
-          resolve(apiFormatada)
-      } 
-      else {
-          reject('Erro na função criarBaralho. Erro: ' + itemDaApi.status);
-      }
-           
-  });
-      
-  let carta = await new Promise(async function (resolve, reject) {
-      
-      let itemDaApi = await fetch(`https://deckofcardsapi.com/api/deck/${baralho.deck_id}/draw/?count=1`);
-       
-        if (itemDaApi.ok) {    
-          let apiFormatada = await itemDaApi.json()          
-          resolve(apiFormatada)
-        } 
-        else {
-          reject('Erro na função tirandoUmaCarta. Erro: ' + itemDaApi.status);
-        }
-         
-  })
-      
-  return carta
-  
-  }
-  
-  catch(error){
-      console.error(error)
-      alert("Aconteceu um erro volte mais tarde.")
-  }
-}
 
 function App(){
 
 let [objCarta,setObjCarta] = useState("Carregando...")
    
 useEffect( ()=>{   
-    async function pegandoCarta(){
-      let carta = await baralhoDeCarta()
-      if(carta){
+
+    async function baralhoDeCarta(){
+        try{
+        
+        let baralho = await new Promise(async function (resolve, reject) {
+           
+            let itemDaApi = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+            
+            if(itemDaApi.ok) {
+                let apiFormatada = await itemDaApi.json();
+                resolve(apiFormatada)
+            } 
+            else {
+                reject('Erro na função criarBaralho. Erro: ' + itemDaApi.status);
+            }
+                 
+        });
+            
+        let carta = await new Promise(async function (resolve, reject) {
+            
+            let itemDaApi = await fetch(`https://deckofcardsapi.com/api/deck/${baralho.deck_id}/draw/?count=1`);
+             
+              if (itemDaApi.ok ) {    
+                let apiFormatada = await itemDaApi.json()          
+                resolve(apiFormatada)
+              } 
+              else {
+                reject('Erro na função tirandoUmaCarta. Erro: ' + itemDaApi.status);
+              }
+               
+        })
+            
         setObjCarta(carta)
+        
+        }
+        
+        catch(error){
+            console.error(error)
+            alert("Aconteceu um erro volte mais tarde.")
+            setObjCarta("Aconteceu um erro volte mais tarde.")
+        }
       }
-      else{
-        setObjCarta("Aconteceu um erro volte mais tarde.")
-      }
-  
-    }   
-    pegandoCarta()
+      baralhoDeCarta()
+
 },[])
 
 return(
